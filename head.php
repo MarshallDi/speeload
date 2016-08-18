6cfa222ca3ab8a98f220ff93198b9156965f2948
flavbleach <flavbleach2@gmail.com>
2016-08-18 08:05:32 +0200

    Minified the original code

--- head.php
+++ head.php
@@ -1,14 +1,1 @@
-<?php include("functions.php");?>
-<html>
-	<head>
-		<link rel="stylesheet" href="https://goo.gl/yAizAi">
-		<link rel="stylesheet" href="m.css">
-		<title>Speeload - Quick & free upload for programmers</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-	</head>
-	<body>
-		<div class="container row">
-			<div class="col l12">
-				<h1><a href="index.php">Speeload</a></h1>
-				<h4>Quickly upload your files</h4>
-			<br/>			
+<?php include("functions.php");?> <html> <head> <link rel="stylesheet" href="https://goo.gl/yAizAi"> <link rel="stylesheet" href="m.css"> <title>Speeload - Quick & free upload for programmers</title> <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"> </head> <body> <div class="container row"> <div class="col l12"> <h1><a href="index.php">Speeload</a></h1> <h4>Quickly upload your files</h4> <br/>
